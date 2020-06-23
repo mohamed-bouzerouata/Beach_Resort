0cba1f85a470de4d4a29d284ab0ddde1f3bc5e73
mohamed-bouzerouata <mohameddevelopper3@gmail.com>
2020-06-23 17:10:59 +0100

    Setup files

---
+++ my-resort/src/Pages/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Helo from Error Component</h1>
+        </div>
+    )
+}
+
+export default Error
+
---
+++ my-resort/src/Pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleRoom = () => {
+    return (
+        <div>
+            <h1>Hello from Single Room Component</h1>
+        </div>
+    )
+}
+
+export default SingleRoom
