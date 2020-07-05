086d33c02e4852cc778ad7dd06b8107d76e28d65
mohamed-bouzerouata <mohameddevelopper3@gmail.com>
2020-07-05 19:57:28 +0100

    Room component

--- my-resort/src/components/Room.js
+++ my-resort/src/components/Room.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import defaultImg from '../images/room-1.jpeg'
+import {Link} from 'react-router-dom'
 
 const Room = ({room}) => {
+    const { name, slug, images , price} = room;
     return (
-        <div className='room'>
+        <article className='room'>
             <div className='img-container'>
-                {room.images}
+                <img src={images[0] || defaultImg }  alt='Single room' />
+                <div className='price-top'>
+                    <h6>{price}$</h6>
+                    <p>Per night</p>
+                </div>
+                <Link to={`/rooms/${slug}`}
+                    className='btn-primary room-link'>
+                        Featured
+                </Link>
             </div>
-        </div>
+        </article>
     )
 }
 
